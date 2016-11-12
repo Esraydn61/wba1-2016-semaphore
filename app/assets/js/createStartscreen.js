ee4ab463afbd1eefbc10a7b4ffa16a52f5a63f88
TimSchuerheck <steinreinfein@googlemail.com>
2016-11-12 16:40:54 +0100

    moved createStartscreen function from fubuizz.js into is file

--- app/assets/js/createStartscreen.js
+++ app/assets/js/createStartscreen.js
@@ -12,31 +12,65 @@ function createStartscreen(quizId){//id, name, author, date, counter, image, des
     console.log("createStartscreeen wurde aufgerufen.");
     console.log("Übergabe: " + quizId);
 
-	// Stylesheet austauschen
-	var sheeturl = urls["startscreen"].replace(/\.html/, ".css");
-	document.getElementById('css-for-view').setAttribute('href', sheeturl);
-	
-    // Snippet ziehen 
-    var template = templates["startscreen"];
-	
-	var quizze = JSON.parse(jsondata["quizubersicht"]);
-	var quiz = quizze[quizId];
+ // JSON Merken
+    var  model.data.rankingjson = JSON.parse(this.responseText);
+		        
+    document.getElementById("content").innerHTML = templates[quizubersicht];
+		        
+	//Snippet des gesamten Startscreens speichern
+	snippetstart = document.getElementById("start");
+    
+	//Snippet der Ranking-Liste speichern
+	snippetranking = document.getElementById("tabellenranking");
+    //Snippet des Ranking_Headers speichern
+	listhead = document.getElementById("listhead");
+		
+	//eventuelle "Eltern" löschen
+	snippetstart.parentNode.removeChild(snippetstart);
+	snippetranking.parentNode.removeChild(snippetranking);
+	listhead.parentNode.removeChild(listhead);
+		  
+	//"reinen" Text in der Variable speichern
+	var template = snippetstart.outerHTML;
+    
+	template = template.replace(/{{name}}/, data.name);
+	template = template.replace(/{{date}}/, data.date);
+	template = template.replace(/{{image}}/, data.image);
+	template = template.replace(/{{description}}/, data.description);
+	template = template.replace(/{{description}}/, data.description);
+		        
+	//"reinen" Text des Listen Headers speichern
+	var htmlRankings = listhead.outerHTML;
+		        
+	//Schleife iteriert durch alle Highscoreeintr#ge des gewählten Quizzes
+	for (var i= 0; i < model.data.rankingjson.highscore.length; i++){
+		
+        //"reinen" Text des Rankings speichern
+        var temp = snippetranking.outerHTML;
 
-	template = template.replace(/{{name}}/, quiz.name);
-    template = template.replace(/{{author}}/, quiz.author);
-    template = template.replace(/{{date}}/, quiz.date);
-    template = template.replace(/{{counter}}/, quiz.counter);
-    template = template.replace(/{{src}}/, quiz.image);
-    template = template.replace(/{{description}}/, quiz.description);
+        temp = temp.replace(/{{rankIdx}}/, model.data.rankingjson.highscore[i].rankIdx);
+        temp = temp.replace(/{{player}}/, model.data.rankingjson.highscore[i].player);
+        temp = temp.replace(/{{points}}/, model.data.rankingjson.highscore[i].points);
+        temp = temp.replace(/{{date}}/, model.data.rankingjson.highscore[i].date);
 
-	var item = document.createElement("div");
-	item.innerHTML = template;
-	item.firstChild.id = quizId;
+        var item = document.createElement("tr");
+        item.innerHTML = temp;
 
-	// HTML in Wrap einfügen
-	document.getElementById("content").replaceChild(item.firstChild, document.getElementById("content").firstChild);
+        htmlRankings += temp;
+    }
 	
-		document.getElementById("playButton").onclick = function(){
-		initQuiz(quiz.quizIdx);
-	}
+    document.getElementById("content").innerHTML = template;
+    document.getElementById("ranking").innerHTML = htmlRankings;
+	  // return this.responseText;
+
+    }
+        
 }
+
+/***********
+highscorecontent
+
+{{rankIdx}}
+{{player}}}
+{{date}}
+{{points}}
